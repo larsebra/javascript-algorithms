392ae5484f5bd4d47d21cce520c8c1a2e0a7e9fb
Lars Erik Bratlie <lars00.brat@gmail.com>
2017-06-07 16:32:34 +0200

    Added BFS and a test. Fixed and added comments

---
+++ test/BFS.js
@@ -0,0 +1,20 @@
+import {expect} from "chai";
+import {compareArray} from "./test-tools/Arrays.js"
+import {breadthFirstSearch} from "../lib/Path Finding/BFS.js"
+
+describe("BFS", function(){
+  it("should find the shortest path in an unwheightet graph", function(){
+    var graph1 = [
+           /*  A   B   C   D   E */
+    /* A */  [-1 , 0 ,-1 ,-1 , 0 ],
+    /* B */  [-1 ,-2 , 0 ,-1 ,-1 ],
+    /* C */  [-1 ,-1 ,-3 , 0 ,-1 ],
+    /* D */  [-1 ,-1 ,-1 ,-4 , 0 ],
+    /* E */  [-1 ,-1 ,-1 ,-1 ,-5 ]
+  ];
+  var BFSShortestPath = breadthFirstSearch(0, 3, graph1);
+  var actualShortestPath = [0,1,2,3];
+  console.log(BFSShortestPath,actualShortestPath);
+  expect(compareArray(actualShortestPath, BFSShortestPath)).to.be.true;
+  })
+});
