bfc6bdf6ccbf2699261f90344dd7b617ea3d76b4
Lars Erik Bratlie <lars00.brat@gmail.com>
2017-06-22 14:13:35 +0200

    moved index.js to root dir

---
+++ index.js
@@ -0,0 +1,48 @@
+/*Linear Structures*/
+import LinkedList from "./src/Data Structures/Linear Structures/LinkedList";
+import DoubleLinkedList from "./src/Data Structures/Linear Structures/DoubleLinkedList";
+import Queue from "./src/Data Structures/Linear Structures/Queue";
+import QueueAsList from "./src/Data Structures/Linear Structures/QueueAsList";
+import Deque from "./src/Data Structures/Linear Structures/Deque";
+import Stack from "./src/Data Structures/Linear Structures/Stack";
+import RingBuffer from "./src/Data Structures/Linear Structures/RingBuffer";
+
+/*Linear Search Algorithms*/
+import BinarySearch from "./src/Search Algorithms/BinarySearch";
+import LinearSearch from "./src/Search Algorithms/LinearSearch";
+
+/*Linear Sort Algorithms*/
+import BubbleSort from "./src/Sort Algorithms/Bubblesort";
+import SelectionSort from "./src/Sort Algorithms/Selectionsort";
+import QuickSort from "./src/Sort Algorithms/Quicksort";
+import MergeSort from "./src/Sort Algorithms/Mergesort";
+
+//Path Finding
+import breadthFirstSearch from "./src/Path Finding/BFS";
+import dijkstras from "./src/Path Finding/BFS";
+import aStar from "./src/Path Finding/A*";
+
+export {
+  /* Linear Datastructures */
+  LinkedList,
+  DoubleLinkedList,
+  Queue,
+  QueueAsList,
+  Deque,
+  RingBuffer,
+
+  /* Search Algorithms */
+  LinearSearch,
+  BinarySearch,
+
+  /* Linear Sort Algorithms */
+  BubbleSort,
+  SelectionSort,
+  QuickSort,
+  MergeSort,
+
+  /* Path Finding */
+  breadthFirstSearch,
+  dijkstras,
+  aStar
+};
